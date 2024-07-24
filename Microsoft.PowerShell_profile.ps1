7bd57d41a544b685e52cdef72691c45341745330
yanqi17118357 <3224848830@qq.com>
2024-07-24 21:58:08 +0800

    feat: fix some bugs and add some function for git

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/di4am0nd.omp.json" | Invoke-Expression
 
-Remove-Alias -Name ni -Force
-Remove-Alias -Name r -Force
+Remove-alias -Name ni -Force
+Remove-alias -Name r -Force
 
 $env:POWERSHELL_UPDATECHECK = 'Off'
 
@@ -19,9 +19,9 @@ function y1 {ssh yanqi@154.12.37.219 -p 23333}
 function u1 {ssh sonna@192.168.40.233}
 function o1 {ssh orangepi@192.168.6.101}
 
-function c {cat $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1}
-function r {notepad $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1}
-function x {rm ‪$env:USERPROFILE\.ssh\known_hosts}
+function c {cat C:\Users\yanqi\Documents\PowerShell\Microsoft.PowerShell_profile.ps1}
+function r {notepad C:\Users\yanqi\Documents\PowerShell\Microsoft.PowerShell_profile.ps1}
+function x {rm C:\Users\yanqi\.ssh\known_hosts}
 
 function .ssh {cat $env:USERPROFILE\.ssh\id_rsa.pub}
 function ssh-copy-id([string]$userAtMachine, $args){   
@@ -56,3 +56,38 @@ function vi($filename) {
 function com {
     wmic path Win32_PnPEntity where "PNPClass = 'Ports'" get Caption
 }
+
+function grt {cd "$(git rev-parse --show-toplevel)"}
+function gs {git status}
+function gp {git push}
+function gpf {git push --force}
+function gpft {git push --follow-tags}
+function gpl {git pull --rebase}
+function gcl {git clone}
+function gst {git stash}
+function grm {git rm}
+function gmv {git mv}
+function main {git checkout main}
+function gco {git checkout}
+function gcob {git checkout -b}
+function gb {git branch}
+function gbd {git branch -d}
+function grb {git rebase}
+function grbom {git rebase origin/master}
+function grbc {git rebase --continue}
+function gl {git log}
+function glo {git log --oneline --graph}
+function grh {git reset HEAD}
+function grh1 {git reset HEAD~1}
+function ga {git add}
+function gA {git add -A}
+function gc {git commit}
+function gcm {git commit -m}
+function gca {git commit -a}
+function gcam {git add -A && git commit -m}
+function gfrb {git fetch origin && git rebase origin/master}
+function gxn {git clean -dn}
+function gx {git clean -df}
+function gsha {git rev-parse HEAD | pbcopy}
+function ghci {gh run list -L 1}
+
